4a90f0f394143d981b38c49dfba10d655c831b48
cidsinga <56556816+cidsinga@users.noreply.github.com>
2020-01-31 09:46:35 -0800

    add seeds for dogs and cats

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_01_31_170236) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cats", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
+    t.boolean "kid_friendly"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "dogs", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
+    t.boolean "kid_friendly"
+    t.integer "age"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+class seed
+  def self.begin
+    seed = Seed.new
+    seed.generate_cats
+  end
+
+  def generate_cats
+    rand(15..35).times do
+      cats = []
+      cats.push(Cats.create!(
+        name: Faker::Movies::StarWars.character
+        color: Faker::Color.color_name
+        kid_friendly: Faker::Boolean.boolean(true_ratio: 0.5)
+        age: Faker::Number.between(from1. to: 15)
+        ))
+      end
+    end
+  end
+  puts "Created #{Cats.count} Cats."
+end
+end
+
+def generate_dogs
+  rand(15..35).times do
+    dogs = []
+    dogs.push(Dogs.create!(
+      name: Faker::Movies::StarWars.character
+      color: Faker::Color.color_name
+      kid_friendly: Faker::Boolean.boolean(true_ratio: 0.5)
+      age: Faker::Number.between(from1. to: 15)
+      ))
+    end
+  end
+end
+puts "Created #{Dogs.count} dogs."
+end
+end
+end
